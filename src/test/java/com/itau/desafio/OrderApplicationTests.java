38ea7921c7a4031c6a6836a44b74dfb6577cd593
Vitor Silva <vitor.procont@gmail.com>
2025-07-22 23:24:57 -0300

    initial commit

---
+++ src/test/java/com/itau/desafio/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
