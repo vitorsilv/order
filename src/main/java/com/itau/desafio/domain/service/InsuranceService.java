cd44825229d660252849ef6bc4d12e62963d4e27
Vitor Silva <vitor.procont@gmail.com>
2025-07-24 00:29:55 -0300

    refactor: estruura de pastas e projeto subindo

--- src/main/java/com/itau/desafio/domain/service/InsuranceService.java
+++ src/main/java/com/itau/desafio/domain/service/InsuranceService.java
@@ -5,6 +5,8 @@ import com.itau.desafio.domain.model.insurance.InsurancePolicy;
 import com.itau.desafio.domain.model.insurance.InsurancePolicyStatus;
 import com.itau.desafio.domain.repository.PolicyRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +14,14 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class PolicyService {
-    private final PolicyRepository policyRepository;
+    private  PolicyRepository policyRepository;
     //CLASSE ANALISE DE FRAUDE
 
     @Transactional
     public InsurancePolicy createPolicy(InsurancePolicy request) {
-        request.setId(UUID.randomUUID());
-        request.setCreatedAt(LocalDateTime.now());
-        request.setStatus(InsurancePolicyStatus.RECEIVED);
-        request.addStatusHistory(InsurancePolicyStatus.RECEIVED);
-
         InsurancePolicy saved = policyRepository.save(request);
         // evento DE NOTIFICACAO
         return saved;
--- src/main/java/com/itau/desafio/infrastructure/rest/InsuranceController.java
+++ src/main/java/com/itau/desafio/infrastructure/rest/InsuranceController.java
@@ -2,6 +2,8 @@ package com.itau.desafio.infrastructure.rest;
 
 import com.itau.desafio.application.PolicyService;
 import com.itau.desafio.domain.model.insurance.InsurancePolicy;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +12,16 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/policies")
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class PolicyController {
-    private final PolicyService policyService;
+    private PolicyService policyService;
 
     @PostMapping
     public ResponseEntity<InsurancePolicy> createPolicy(
             @RequestBody InsurancePolicy request
     ) {
-//        return ResponseEntity.ok(policyService.createPolicy(request));
-        return ResponseEntity.ok(new InsurancePolicy());
+        return ResponseEntity.ok(policyService.createPolicy(request));
     }
 
 }
